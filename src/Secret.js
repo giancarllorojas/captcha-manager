5e7c89b56eff87bd9e6208b9d3ae0861464fd8b8
samjmckenzie <sammckenzies.p@gmail.com>
2017-10-16 13:37:02 +0200

    Revert previous changes

--- src/Secret.js
+++ src/Secret.js
@@ -1,22 +1,14 @@
 'use strict';
 
 const fs = require('fs');
-const argv = require('yargs').argv;
 const crypto = require('crypto');
 
 const path = __dirname + '/.secret';
 
-let secret = null;
-
 if(fs.existsSync(path)){
-    secret = fs.readFileSync(path, 'utf8');
+    return module.exports = fs.readFileSync(path, 'utf8');
 }
-if(secret === null){
-    secret = crypto.randomBytes(128).toString('hex');
-    fs.writeFileSync(path, secret, 'utf8');
-}
-if(argv.logSecret !== undefined){
-    return console.log(secret);
-}
+const secret = crypto.randomBytes(128).toString('hex');
+fs.writeFileSync(path, secret, 'utf8');
 
 module.exports = secret;
